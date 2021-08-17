944da211655c70ced2ad9879c7207343f55f6d60
Reem Essam <reem1252001@hotmail.com>
2021-08-18 00:13:49 +0200

    .

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -1,4 +1,7 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:hayel_gocloud/firstPage.dart';
 
 class LoginScreen extends StatelessWidget {
   final EmailController = TextEditingController();
@@ -11,6 +14,7 @@ class LoginScreen extends StatelessWidget {
     if (enteredEmail.isEmpty || enteredPass.isEmpty) {
       return;
     }
+
   }
 
   @override
@@ -18,86 +22,63 @@ class LoginScreen extends StatelessWidget {
     final mediaQuery = MediaQuery.of(context);
     return Scaffold(
         backgroundColor: Colors.white70,
-        body: Container(
-            width: (mediaQuery.size.width) * 0.8,
-            height: (mediaQuery.size.height - mediaQuery.padding.top) * 0.8,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black54,
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                )
-              ],
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(50, 255, 100, 1).withOpacity(0.5),
-                  Color.fromRGBO(70, 117, 100, 1).withOpacity(0.9),
+        body: SingleChildScrollView(
+          child: Container(
+              width: (mediaQuery.size.width) * 0.8,
+              height: (mediaQuery.size.height - mediaQuery.padding.top) * 0.8,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                  )
                 ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0, 1],
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(50, 255, 100, 1).withOpacity(0.5),
+                    Color.fromRGBO(70, 117, 100, 1).withOpacity(0.9),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0, 1],
+                ),
               ),
-            ),
-            margin: EdgeInsets.only(
-                top: (mediaQuery.size.height - mediaQuery.padding.top) * 0.15,
-                left: (mediaQuery.size.width) * 0.1),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    child: ListTile(
-                  leading: Icon(Icons.login),
-                  title: Text(
-                    'enter your information below',
-                    textScaleFactor: 1.5,
-                    style: TextStyle(color: Colors.black26),
+              margin: EdgeInsets.only(
+                  top: (mediaQuery.size.height - mediaQuery.padding.top) * 0.15,
+                  left: (mediaQuery.size.width) * 0.1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                //mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      child: ListTile(
+                    leading: Icon(Icons.login),
+                    title: Text(
+                      'enter your information below',
+                      textScaleFactor: 1.5,
+                      style: TextStyle(color: Colors.black26),
+                    ),
+                    subtitle: Text(
+                      'Hayel App',
+                      textScaleFactor: 1,
+                      style: TextStyle(color: Colors.black26),
+                    ),
+                  )),
+                  SizedBox(
+                    height: 50,
+                    child: Divider(
+                      color: Colors.green[300],
+                    ),
                   ),
-                  subtitle: Text(
-                    'Hayel App',
-                    textScaleFactor: 1,
-                    style: TextStyle(color: Colors.black26),
-                  ),
-                )),
-                SizedBox(
-                  height: 50,
-                  child: Divider(
-                    color: Colors.green[300],
-                  ),
-                ),
-                SizedBox(
-                  width: 250,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(
-                            color: Colors.black26, fontWeight: FontWeight.bold),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black12),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5.0))),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green[300]),
-                        )),
-                    controller: EmailController,
-                    onSubmitted: (_) => submitData(),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
+                  SizedBox(
                     width: 250,
                     child: TextField(
-                      obscureText: true,
-                      // cursorColor: Colors.green[500],
                       decoration: InputDecoration(
-                          labelText: 'Password',
+                          labelText: 'Email',
                           labelStyle: TextStyle(
-                              color: Colors.black26,
-                              fontWeight: FontWeight.bold),
+                              color: Colors.black26, fontWeight: FontWeight.bold),
                           enabledBorder: OutlineInputBorder(
                               borderSide: BorderSide(color: Colors.black12),
                               borderRadius:
@@ -105,41 +86,70 @@ class LoginScreen extends StatelessWidget {
                           focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.green[300]),
                           )),
-                      controller: PasswordController,
+                      controller: EmailController,
                       onSubmitted: (_) => submitData(),
-                    )),
-                SizedBox(
-                  height: 15,
-                ),
-                SizedBox(
-                    width: 250,
-                    child: ElevatedButton(
-                      child: Text('Login'),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.green[500],
-                      ),
-                      onPressed: submitData,
-                    )),
-                SizedBox(
-                  height: 10,
-                ),
-                Divider(
-                  color: Colors.green[300],
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: TextButton(
-                    onPressed: submitData,
-                    child: Text(
-                      'forgot password',
-                      style: TextStyle(
-                          color: Colors.black26,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15),
                     ),
                   ),
-                )
-              ],
-            )));
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                      width: 250,
+                      child: TextField(
+                        obscureText: true,
+                        // cursorColor: Colors.green[500],
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                                color: Colors.black26,
+                                fontWeight: FontWeight.bold),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black12),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5.0))),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green[300]),
+                            )),
+                        controller: PasswordController,
+                        onSubmitted: (_) => submitData(),
+                      )),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                      width: 250,
+                      child: ElevatedButton(
+                        child: Text('Login'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green[500],
+                        ),
+                        onPressed: (){
+                          submitData();
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (BuildContext ctx) => FirstPage()));
+                        },
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Divider(
+                    color: Colors.green[300],
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: TextButton(
+                      onPressed: submitData,
+                      child: Text(
+                        'forgot password',
+                        style: TextStyle(
+                            color: Colors.black26,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15),
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        ));
   }
 }
---
+++ lib/firstPage.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class FirstPage extends StatefulWidget {
+  FirstPage({Key key}) : super(key: key);
+
+
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+
+  final GlobalKey<ScaffoldState> _drawerKey = new GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+
+    Widget selectedPage = HomePage();
+    return Scaffold(
+      appBar: AppBar(
+          leading: Icon(Icons.circle)
+      ),
+
+      body: Scaffold(
+        key: _drawerKey,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              //SizedBox(height: 15,),
+              ListTile(
+                title: const Text("Home"),
+                leading: Icon(Icons.home, color: Colors.black,),
+                onTap: ()
+                {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Attend By Face"),
+                leading: Icon(Icons.person, color: Colors.black,),
+                onTap: ()
+                {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Show QR"),
+                leading: Icon(Icons.qr_code, color: Colors.black,),
+                onTap: ()
+                {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Departments"),
+                leading: Icon(Icons.apartment, color: Colors.black,),
+                onTap: ()
+                {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Employees"),
+                leading: Icon(Icons.people_alt, color: Colors.black,),
+                onTap: ()
+                {
+                  Navigator.pop(context);
+                },
+              ),
+              ExpansionTile(
+                title: const Text("Message"),
+                leading: Icon(Icons.add_alert_rounded, color: Colors.black,),
+                children: [
+                  ListTile(
+                    title: const Text("Send New Message"),
+                    leading: Icon(Icons.add_alert_rounded, color: Colors.black,),
+                    onTap: ()
+                    {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("All Messages"),
+                    leading: Icon(Icons.filter_frames_sharp, color: Colors.black,),
+                    onTap: ()
+                    {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: const Text("Reports"),
+                leading: Icon(Icons.add_alert_rounded, color: Colors.black,),
+                children: [
+                  ListTile(
+                    title: const Text("Send New Message"),
+                    leading: Icon(Icons.add_alert_rounded, color: Colors.black,),
+                    onTap: ()
+                    {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text("All Messages"),
+                    leading: Icon(Icons.filter_frames_sharp, color: Colors.black,),
+                    onTap: ()
+                    {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        body:SingleChildScrollView(
+          child: Center(
+            child: Column(
+
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                    child: IconButton(
+                        icon: Icon(Icons.menu),
+                        onPressed: (){
+                          if (_drawerKey.currentState.isDrawerOpen)
+                          {
+                            Navigator.pop(context);
+                          }
+                          else{
+                            _drawerKey.currentState.openDrawer();
+                          }
+                        }
+                    ),
+                    decoration: BoxDecoration(border: Border.all(color: Colors.black12), borderRadius: BorderRadius.circular(5)),
+                  ),
+                  color: Colors.white,
+                )
+                ,
+                selectedPage
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
